9b0b9859961fcbfe8e0afe43f5c26af8e2b82562
Kate Owens <kowens711@gmail.com>
2017-04-11 09:07:43 -0400

    First commit

---
+++ Squarer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Squarer
+//
+//  Created by Katherine Owens on 4/11/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
